2c0cd14fd7ec690afbac5d1e97019cc21be9ccab
Alper Reha YAZGAN <alperreha@gmail.com>
2022-03-28 19:17:48 +0300

    dockercompose created. config migrate to config.go

---
+++ config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// Env variables config
+var (
+	APP_PORT      = os.Getenv("APP_PORT")
+	DBCONNSTR     = os.Getenv("DBCONNSTR")
+	NATS_URL      = os.Getenv("NATS_URL")
+	REDISCONNSTR  = os.Getenv("REDISCONNSTR")
+	S3_REGION     = os.Getenv("S3_REGION")
+	S3_BUCKET     = os.Getenv("S3_BUCKET")
+	S3_ACCESS_KEY = os.Getenv("S3_ACCESS_KEY")
+	S3_SECRET_KEY = os.Getenv("S3_SECRET_KEY")
+	S3_ENDPOINT   = os.Getenv("S3_ENDPOINT")
+	S3_PUBLIC     = os.Getenv("S3_PUBLIC")
+)
+
+func LoadEnv(dirname string) {
+	// Load environment variables
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// get env variables
+	APP_PORT = os.Getenv("APP_PORT")
+	DBCONNSTR = os.Getenv("DBCONNSTR")
+	NATS_URL = os.Getenv("NATS_URL")
+	REDISCONNSTR = os.Getenv("REDISCONNSTR")
+	S3_PUBLIC = os.Getenv("S3_BUCKET")
+	S3_REGION = os.Getenv("S3_REGION")
+	S3_BUCKET = os.Getenv("S3_BUCKET")
+	S3_ACCESS_KEY = os.Getenv("S3_ACCESS_KEY")
+	S3_SECRET_KEY = os.Getenv("S3_SECRET_KEY")
+	S3_ENDPOINT = os.Getenv("S3_ENDPOINT")
+
+}
--- main.go
+++ main.go
@@ -8,70 +8,41 @@ import (
 
 	// web framework
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 
 	// local packages
+	config "ttcertman/config"
 	db "ttcertman/src/platform/database"
 )
 
-// Env variables config
-var (
-	APP_PORT      = os.Getenv("APP_PORT")
-	DBCONNSTR     = os.Getenv("DBCONNSTR")
-	NATS_URL      = os.Getenv("NATS_URL")
-	REDISCONNSTR  = os.Getenv("REDISCONNSTR")
-	S3_REGION     = os.Getenv("S3_REGION")
-	S3_BUCKET     = os.Getenv("S3_BUCKET")
-	S3_ACCESS_KEY = os.Getenv("S3_ACCESS_KEY")
-	S3_SECRET_KEY = os.Getenv("S3_SECRET_KEY")
-	S3_ENDPOINT   = os.Getenv("S3_ENDPOINT")
-	S3_PUBLIC     = os.Getenv("S3_PUBLIC")
-)
-
-func LoadEnv() {
-	// Load environment variables
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
+func InitEnv() {
 	// get current directory
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// get env variables
-	APP_PORT = os.Getenv("APP_PORT")
-	DBCONNSTR = os.Getenv("DBCONNSTR")
-	NATS_URL = os.Getenv("NATS_URL")
-	REDISCONNSTR = os.Getenv("REDISCONNSTR")
-	S3_PUBLIC = os.Getenv("S3_BUCKET")
-	S3_REGION = os.Getenv("S3_REGION")
-	S3_BUCKET = os.Getenv("S3_BUCKET")
-	S3_ACCESS_KEY = os.Getenv("S3_ACCESS_KEY")
-	S3_SECRET_KEY = os.Getenv("S3_SECRET_KEY")
-	S3_ENDPOINT = os.Getenv("S3_ENDPOINT")
+	// load env variables from
+	config.LoadEnv(dir)
 
 	// init database
-	db.InitDatabaseConnection(dir + "/" + DBCONNSTR)
+	db.InitDatabaseConnection(dir + "/" + config.DBCONNSTR)
 	// init migrations
 	db.InitMigrations()
 }
 
 func run() error {
-	// Load environment variables
-	LoadEnv()
+	// Init environment variables
+	InitEnv()
 
 	// Create a new gin router
 	r := gin.Default()
 	Routes(r)
 
 	// start server on port APP_PORT
-	if APP_PORT == "" {
-		APP_PORT = "9090"
+	if config.APP_PORT == "" {
+		config.APP_PORT = "9090"
 	}
-	return r.Run(":" + APP_PORT)
+	return r.Run(":" + config.APP_PORT)
 }
 
 func main() {
